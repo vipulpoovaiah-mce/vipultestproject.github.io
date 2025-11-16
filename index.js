87d1c87beb3e92df54baf2112884dce9bc5b0d8f
vipulpoovaiah-mce <vipulpoovaiah.m.v.mce@gmail.com>
2025-11-16 12:06:39 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-
-let name;
-
-document.getElementById("Username_submit").onclick = function() {
-  name = document.getElementById("myUserName").value;
-  console.log(name);
-  document.getElementById("change_title").textContent = `Hello ${name}`;
-}
